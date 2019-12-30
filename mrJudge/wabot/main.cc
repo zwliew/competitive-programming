b13deb4c9b88c01b54945d94ed06d7cc05e30262
Zhao Wei Liew <zhaoweiliew@gmail.com>
2019-12-30 21:33:50 +0800

    mrJudge: Add wabot

---
+++ mrJudge/wabot/main.cc
@@ -0,0 +1,98 @@
+/*
+ID: zhaowei10
+TASK: test
+LANG: C++
+*/
+// #include <bits/stdc++.h>
+#include <algorithm>
+#include <array>
+#include <climits>
+#include <cmath>
+#include <deque>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+#define MOD 1000000007
+#define FOR(i, j, k, l) for (s32 i = j; i < k; i += l)
+#define RFOR(i, j, k, l) for (s32 i = j; i >= k; i -= l)
+#define REP(i, j) FOR(i, 0, j, 1)
+#define RREP(i, j) RFOR(i, j, 0, 1)
+#define ALL(x) x.begin(), x.end()
+#define MEAN(a, b) min(a, b) + (abs(b - a) >> 1)
+#define FASTIO()    \
+  cin.tie(nullptr); \
+  ios::sync_with_stdio(false);
+
+typedef int64_t s64;
+typedef uint64_t u64;
+typedef int32_t s32;
+typedef uint32_t u32;
+typedef float f32;
+typedef double f64;
+typedef pair<s32, s32> II;
+typedef pair<s64, s64> LL;
+typedef vector<s32> VI;
+typedef vector<II> VII;
+typedef vector<LL> VLL;
+typedef vector<char> VC;
+typedef priority_queue<s32> PQI;
+typedef priority_queue<s32, VI, greater<s32>> PQIA;
+typedef priority_queue<s64> PQL;
+typedef priority_queue<II> PQII;
+typedef priority_queue<II, VII, greater<II>> PQIIA;
+typedef priority_queue<LL> PQLL;
+typedef priority_queue<LL, VLL, greater<LL>> PQLLA;
+typedef unordered_set<s32> USI;
+typedef unordered_set<s64> USL;
+typedef set<II> SII;
+typedef multiset<s32> MSI;
+
+template <typename T, typename U>
+inline void amin(T &x, U y) {
+  if (y < x) x = y;
+}
+template <typename T, typename U>
+inline void amax(T &x, U y) {
+  if (x < y) x = y;
+}
+
+int main() {
+  FASTIO();
+
+  s64 n;
+  cin >> n;
+  PQLLA q;
+  while (n--) {
+    s64 l, c;
+    cin >> l >> c;
+    q.emplace(l, c);
+  }
+  auto p = q.top();
+  q.pop();
+  s64 t = 0;
+  while (!q.empty()) {
+    auto c = q.top();
+    q.pop();
+    if (c.first != p.first) {
+      p = c;
+      continue;
+    }
+    t += c.second + p.second;
+    q.emplace(c.first + 1, c.second + p.second);
+    p = q.top();
+    q.pop();
+  }
+
+  cout << t;
+}
