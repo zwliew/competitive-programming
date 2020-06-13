1ef89ad802588d1d5cce43db76a57c5ed3a5f430
Zhao Wei Liew <zhaoweiliew@gmail.com>
2020-06-13 13:55:16 +0800

    Create a GCC compilation shell script

---
+++ gcc.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+file=${1:-"a"}
+
+g++ -I_library/cpp -DLOCAL -static -pipe -O2 -std=c++17 -Wall -Wextra -pedantic -Wshadow -Wformat=2 -Wfloat-equal -Wconversion -Wlogical-op -Wshift-overflow=2 -Wduplicated-cond -Wcast-qual -Wcast-align -D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC -D_FORTIFY_SOURCE=2 -fno-sanitize-recover -fstack-protector _work/cpp/$file.cc; ./a.out
