546cc2020281c5497c42610f7a4a230cee79a5bc
Zhao Wei Liew <zhaoweiliew@gmail.com>
2020-06-18 10:17:05 +0800

    Rename OrderStatisticTree

--- _library/cpp/data-structures/OrderStatisticTree.h
+++ _library/cpp/data-structures/OrderStatisticTree.h
@@ -2,6 +2,6 @@
 #include <ext/pb_ds/tree_policy.hpp>
 
 template <class T>
-using Tree =
+using OrderStatisticTree =
     __gnu_pbds::tree<T, __gnu_pbds::null_type, less<T>, __gnu_pbds::rb_tree_tag,
                      __gnu_pbds::tree_order_statistics_node_update>;
