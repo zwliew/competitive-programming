9146a26448216571a5a67d821bcef7e3e69d5221
Zhao Wei Liew <zhaoweiliew@gmail.com>
2021-05-23 08:58:52 +0800

    template/cc: move namespace usage to main function

--- _templates/template.cc
+++ _templates/template.cc
@@ -1,5 +1,4 @@
 #include "bits/stdc++.h"
-using namespace std;
 #define debug(...) 0
 #ifdef LOCAL
 #include "../../_library/cc/debug.h"
@@ -7,6 +6,7 @@ using namespace std;
 // #define FILE
 
 int main() {
+  using namespace std;
   cin.tie(nullptr)->sync_with_stdio(false);
 #if defined(FILE) && !defined(LOCAL)
   freopen(FILE ".in", "r", stdin), freopen(FILE ".out", "w", stdout);
