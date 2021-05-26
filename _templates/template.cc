dd4da3c9b0124596b807bc4a01a397d67e289861
Zhao Wei Liew <zhaoweiliew@gmail.com>
2021-05-26 20:23:49 +0800

    _templates/cc: improve template usability

--- _templates/template.cc
+++ _templates/template.cc
@@ -3,12 +3,16 @@
 #ifdef LOCAL
 #include "../../_library/cc/debug.h"
 #endif
+using namespace std;
+void solve();
 // #define FILE
 
 int main() {
-  using namespace std;
   cin.tie(nullptr)->sync_with_stdio(false);
 #if defined(FILE) && !defined(LOCAL)
   freopen(FILE ".in", "r", stdin), freopen(FILE ".out", "w", stdout);
 #endif
+  solve();
 }
+
+void solve() {}
